96167d5414bd665290f4b740d76f70695b96736f
Garrix00 <127745342+Garrix00@users.noreply.github.com>
2023-03-13 15:51:44 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf ("yoo sono antonio e piscio in testa al sason\n");
+}
---
+++ inizio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int     numero = 42;
+    double  numerolungo = 4536274;
+    float   numeroconlavirgola = 4.545;
+    char    carattere = 'c';
+
+    printf("il numero è: %d\n", numero);
+    printf("numero lungo è: %f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola);
+    printf("ïl carattere è %c\n", carattere);
+}
