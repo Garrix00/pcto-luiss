8a29a1ea8158f61fb541d3f4a39306a785936b89
Garrix00 <127745342+Garrix00@users.noreply.github.com>
2023-03-16 10:16:39 +0100

    Add files via upload

---
+++ nazibabbo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i =0;
+    int n;
+    printf("inserisci un numero\n");
+    scanf("%d", &n);
+    int array[n];
+    int j;
+    int k;
+    while( i < n )
+    {
+        printf("inserisci un numero\n");
+        scanf("%d", &array[i]);
+        i++;
+    }
+    i=1;
+    j=array[0];
+    k=array[0];
+    while(i<n)
+    {
+        if(array[i]<j)
+        {
+            j = array[i];
+        }
+        else if(array[i]>k)
+        {
+            k=array[i];
+        }
+        i++;
+    }
+    printf("il numero piú piccolo è %d\n il numero piú grande è %d\n",j, k);
+
+}
